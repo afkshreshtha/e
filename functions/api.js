560a1c06a92430fc5140ca98210929bbc902f572
afkshreshtha <shreshthaagarwal1234@gmail.com>
2024-08-04 14:27:58 +0530

    h

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
   const response = {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "http://localhost:3000"
     },
     body: JSON.stringify({ message: "Hello World" })
   };
