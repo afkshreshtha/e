e3fe158b6129b9a0e2aa0b079b7a31c33d6a2651
afkshreshtha <shreshthaagarwal1234@gmail.com>
2024-08-04 16:37:59 +0530

    gg

--- functions/api.js
+++ functions/api.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const ffmpeg = require('fluent-ffmpeg');
-const axios = require('axios');
-const serverless = require('serverless-http');
-const cors = require('cors');
-const fs = require('fs');
-const tmp = require('tmp');
-const path = require('path');
-const ffmpegPath = require('ffmpeg-static');
+const express = require('express')
+const axios = require('axios')
+const serverless = require('serverless-http')
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
+const ffmpegPath = require('ffmpeg-static')
+const { exec } = require('child_process')
+const execPromise = util.promisify(exec)
+const app = express()
+const router = express.Router()
+const TEMP_DIR = '/tmp'
 
-const app = express();
-const router = express.Router();
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+)
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
-
-app.use(express.json());
+app.use(express.json())
 
 // Set the path to the ffmpeg binary using ffmpeg-static
-ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfmpegPath(ffmpegPath)
 
 // Function to download a file from a URL into a buffer
 const downloadFileToBuffer = async (url) => {
@@ -26,81 +28,90 @@ const downloadFileToBuffer = async (url) => {
     url,
     method: 'GET',
     responseType: 'arraybuffer',
-  });
-  return response.data;
-};
+  })
+  return response.data
+}
 
 router.post('/convert', async (req, res) => {
-  const { audioUrl, imageUrl, artists, album } = req.body;
+  const { audioUrl, imageUrl, artists, album } = req.body
+  const audioPath = path.join(TEMP_DIR, 'input.mp4')
+  const imagePath = path.join(TEMP_DIR, 'cover.jpg')
+  const outputPath = path.join(TEMP_DIR, 'output.mp3')
 
   if (!audioUrl || !imageUrl || !artists || !album) {
-    return res.status(400).json({ error: 'Audio URL, Image URL, artist name, and album name are required' });
+    return res.status(400).json({
+      error: 'Audio URL, Image URL, artist name, and album name are required',
+    })
   }
 
-  console.log("Audio URL: " + audioUrl)
-  console.log("Image URL: " + imageUrl)
-  console.log("Artists: " + artists)
-  console.log("Album: " + album)
-  
+  console.log('Audio URL: ' + audioUrl)
+  console.log('Image URL: ' + imageUrl)
+  console.log('Artists: ' + artists)
+  console.log('Album: ' + album)
+
   try {
-    const audioBuffer = await downloadFileToBuffer(audioUrl);
-    const imageBuffer = await downloadFileToBuffer(imageUrl);
+    // Log FFmpeg path
+    console.log('FFmpeg path:', ffmpegPath)
 
-    const audioTmpFile = tmp.fileSync();
-    const imageTmpFile = tmp.fileSync();
-    const outputTmpFile = tmp.fileSync();
+    // Check if the FFmpeg binary exists
+    if (!fs.existsSync(ffmpegPath)) {
+      console.error('FFmpeg binary not found at', ffmpegPath)
+      return res.status(500).json({ error: 'FFmpeg binary not found' })
+    }
 
-    fs.writeFileSync(audioTmpFile.name, audioBuffer);
-    fs.writeFileSync(imageTmpFile.name, imageBuffer);
+    // Download audio file
+    const audioResponse = await axios({
+      url: audioUrl,
+      method: 'GET',
+      responseType: 'stream',
+    })
 
-    console.log('AudioTmpFile' + ": " + audioTmpFile)
-    console.log('ImageTmpFile' + ": " + imageTmpFile)
+    await new Promise((resolve, reject) => {
+      const audioWriter = fs.createWriteStream(audioPath)
+      audioResponse.data.pipe(audioWriter)
+      audioWriter.on('finish', resolve)
+      audioWriter.on('error', reject)
+    })
+    // Download image file
+    const imageResponse = await axios({
+      url: imageUrl,
+      method: 'GET',
+      responseType: 'stream',
+    })
 
-    ffmpeg()
-      .input(audioTmpFile.name)
-      .input(imageTmpFile.name)
-      .inputFormat('m4a') // Specify input format if needed
-      .outputOptions('-map', '0:a') // Map audio stream
-      .outputOptions('-map', '1') // Map image stream
-      .outputOptions('-c:v', 'mjpeg') // Video codec for image
-      .outputOptions('-id3v2_version', '3') // ID3 version for metadata
-      .outputOptions('-metadata:s:v', 'title="Album cover"') // Metadata for cover image
-      .outputOptions('-metadata:s:v', 'comment="Cover (front)"')
-      .outputOptions('-metadata', `artist="${artists}"`) // Metadata for artist
-      .outputOptions('-metadata', `album="${album}"`) // Metadata for album
-      .audioCodec('libmp3lame') // Audio codec for MP3
-      .toFormat('mp3') // Output format
-      .on('start', (cmd) => {
-        console.log('Started ffmpeg with command:', cmd);
-      })
-      .on('end', () => {
-        console.log('Conversion finished');
-        const outputBuffer = fs.readFileSync(outputTmpFile.name);
-        res.set('Content-Type', 'audio/mpeg');
-        res.set('Content-Disposition', 'attachment; filename=output.mp3');
-        res.send(outputBuffer);
+    await new Promise((resolve, reject) => {
+      const imageWriter = fs.createWriteStream(imagePath)
+      imageResponse.data.pipe(imageWriter)
+      imageWriter.on('finish', resolve)
+      imageWriter.on('error', reject)
+    })
 
-        // Clean up temporary files
-        audioTmpFile.removeCallback();
-        imageTmpFile.removeCallback();
-        outputTmpFile.removeCallback();
-      })
-      .on('error', (err) => {
-        console.error('Error during conversion:', err.message);
-        res.status(500).json({ error: 'Conversion failed' });
+    console.log('Audio file exists:', fs.existsSync(audioPath))
+    console.log('Image file exists:', fs.existsSync(imagePath))
 
-        // Clean up temporary files on error
-        audioTmpFile.removeCallback();
-        imageTmpFile.removeCallback();
-        outputTmpFile.removeCallback();
-      })
-      .save(outputTmpFile.name);
+    // Execute ffmpeg command
+    await execPromise(
+      `${ffmpegPath} -i ${audioPath} -i ${imagePath} -c:v mjpeg -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" -metadata artist="${artists.join(
+        ', ',
+      )}" -metadata album="${album}" ${outputPath}`,
+    )
+    const fileData = fs.readFileSync(outputPath)
+    fs.unlinkSync(audioPath)
+    fs.unlinkSync(imagePath)
+    fs.unlinkSync(outputPath)
 
+    return res.status(200).json({
+      audioUrl,
+      imageUrl,
+      artists,
+      album,
+      audioData: fileData.toString('base64'),
+    })
   } catch (err) {
-    console.error('Error in the process:', err.message);
-    res.status(500).json({ error: 'Process failed' });
+    console.error('Conversion Failed', err.message)
+    res.status(500).json({ error: 'Conversation Failed' })
   }
-});
+})
 
-app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app)
